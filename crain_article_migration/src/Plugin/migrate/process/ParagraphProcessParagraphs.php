4165b4f8588c54d7debdd2e27f03c9e43cdf3c15
vagrant <vagrant@local.crain-platform.com>
2017-11-07 10:12:10 +0000

    process paragraphs

---
+++ crain_article_migration/src/Plugin/migrate/process/ParagraphProcessParagraphs.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\crain_article_migration\Plugin\migrate\process\ParagraphProcessParagraphs.
+ */
+
+namespace Drupal\crain_article_migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drupal\node\Entity\Node;
+use Drupal\paragraphs\Entity\Paragraph;
+
+/**
+ *
+ * @MigrateProcessPlugin(
+ *   id = "paragraph_process_paragraphs"
+ * )
+ */
+class ParagraphProcessParagraphs extends ProcessPluginBase {
+  	/**
+  	 * {@inheritdoc}
+  	 */
+   	public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+      $factbox = $row->getDestinationProperty('paragraph_factbox');
+      $body = $row->getDestinationProperty('paragraph_body');
+      $photographs = $row->getDestinationProperty('paragraph_photographs');
+      return array_merge($body,$factbox,$photographs);
+    }
+}
